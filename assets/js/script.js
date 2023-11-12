a48665b510403bb935d15b067c57521c763f13fb
ChipMasterVN <chipmastervn@gmail.com>
2023-11-12 16:46:38 +0700

    Add Relevant info

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,6 +139,27 @@
       },
     ],
   });
+  
+  // relevant-slider
+  $(".relevant-slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    arrows: false,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
   // clients logo slider
   $(".client-logo-slider").slick({
